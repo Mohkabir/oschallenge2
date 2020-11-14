2b56ce6755bc6ef79e2cbd4c47056ff4b77bca32
Kabir <60716161+Mohkabir@users.noreply.github.com>
2020-11-14 09:04:15 -1200

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-$(document).ready(function(){
-  $('.menu').click(function(){
+$(document).ready(function () {
+  $('.menu').click(function () {
     $('.mainNav').slideToggle()
   });
 
-    $('#top').click(function(){
+  $('#top').click(function () {
     var userLocation = window.location.href;
-  var currentUrl = userLocation.substring(userLocation.lastIndexOf('#')+1);
+    var currentUrl = userLocation.substring(userLocation.lastIndexOf('#') + 1);
 
-  if(currentUrl == 'home'){
-    $('html, body').animate({
-      scrollTop: $('#scroll-' + currentUrl).offset().top
-    },1000);
-  }
+    if (currentUrl == 'home') {
+      $('html, body').animate({
+        scrollTop: $('#scroll-' + currentUrl).offset().top
+      }, 1000);
+    }
   })
-  
+
 });
 
 
-window.onload = function(){
+window.onload = function () {
 
   let getImgWindow = document.querySelector('.bg-img');
   let getTracker1 = document.querySelector('.tracker_1');
@@ -27,65 +27,60 @@ window.onload = function(){
   let currentImg = 1;
 
 
-  setInterval(imgTransform,3000);
+  setInterval(imgTransform, 3000);
 
-  function imgTransform(){
-    if(currentImg === 1){
-      getImgWindow.setAttribute('src','./images/bg1.jpg');
+  function imgTransform() {
+    if (currentImg === 1) {
+      getImgWindow.setAttribute('src', './images/bg1.jpg');
       getTracker1.style.backgroundColor = '#272727';
       getTracker2.style.backgroundColor = '#686868';
       getTracker3.style.backgroundColor = '#686868';
-    }else if(currentImg === 2){
-      getImgWindow.setAttribute('src','./images/bg2.jpg');
+    } else if (currentImg === 2) {
+      getImgWindow.setAttribute('src', './images/bg2.jpg');
       getTracker2.style.backgroundColor = '#272727';
       getTracker1.style.backgroundColor = '#686868';
       getTracker3.style.backgroundColor = '#686868';
-    }else if(currentImg === 3){
-      getImgWindow.setAttribute('src','./images/bg3.jpg');
+    } else if (currentImg === 3) {
+      getImgWindow.setAttribute('src', './images/bg3.jpg');
       getTracker3.style.backgroundColor = '#272727';
       getTracker1.style.backgroundColor = '#686868';
       getTracker2.style.backgroundColor = '#686868';
     }
 
-    currentImg++ ;
+    currentImg++;
 
-    if(currentImg > 3){
-      currentImg = 1 ;
-    }else if(currentImg < 1){
+    if (currentImg > 3) {
+      currentImg = 1;
+    } else if (currentImg < 1) {
       currentImg = 3;
     }
   }
-  getTracker1.onclick = function(){
+  getTracker1.onclick = function () {
     currentImg = 1;
   }
-  getTracker2.onclick = function(){
+  getTracker2.onclick = function () {
     currentImg = 2;
   }
-  getTracker3.onclick = function(){
+  getTracker3.onclick = function () {
     currentImg = 3;
   }
 
 
 
+  setInterval(textChanger, 3000);
 
-
-
-
-
-  setInterval(textChanger,3000);
-
-  let currentText = 1 ;
+  let currentText = 1;
   let getChange1 = document.querySelector('.change_1');
   let getChange2 = document.querySelector('.change_2');
   let getChange3 = document.querySelector('.change_3');
-  
+
   let getChangeTracker1 = document.querySelector('.change_tracker1');
   let getChangeTracker2 = document.querySelector('.change_tracker2');
   let getChangeTracker3 = document.querySelector('.change_tracker3');
 
-  
-  function textChanger(){
-    if(currentText === 1){
+
+  function textChanger() {
+    if (currentText === 1) {
       getChange1.style.display = 'block';
       getChange2.style.display = 'none';
       getChange3.style.display = 'none';
@@ -93,7 +88,7 @@ window.onload = function(){
       getChangeTracker1.style.backgroundColor = '#ffffff';
       getChangeTracker2.style.backgroundColor = '#686868';
       getChangeTracker3.style.backgroundColor = '#686868';
-    }else if(currentText === 2){
+    } else if (currentText === 2) {
       getChange1.style.display = 'none';
       getChange2.style.display = 'block';
       getChange3.style.display = 'none';
@@ -101,7 +96,7 @@ window.onload = function(){
       getChangeTracker1.style.backgroundColor = '#686868';
       getChangeTracker3.style.backgroundColor = '#686868';
       getChangeTracker2.style.backgroundColor = '#ffffff';
-    }else if(currentText === 3){
+    } else if (currentText === 3) {
       getChange1.style.display = 'none';
       getChange2.style.display = 'none';
       getChange3.style.display = 'block';
@@ -110,10 +105,10 @@ window.onload = function(){
       getChangeTracker2.style.backgroundColor = '#686868';
       getChangeTracker3.style.backgroundColor = '#ffffff';
     }
-    currentText++ ;
+    currentText++;
 
-    if(currentText > 3){
-      currentText = 1 ;
+    if (currentText > 3) {
+      currentText = 1;
     }
   }
 
@@ -124,4 +119,19 @@ window.onload = function(){
 
 
 
+}
+
+function transform() {
+  let getb = document.body;
+  let getSec = document.querySelector('.section_4');
+
+  if (getb.classList.contains('.dark')) {
+    getb.style.backgroundColor = '#fff';
+    getb.classList.remove('.dark');
+  } else {
+    getb.style.backgroundColor = '#272727';
+    getb.style.color = '#fff';
+    getSec.style.backgroundColor = '#272727';
+    getb.classList.add('.dark');
+  }
 }
